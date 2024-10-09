cbdf96bca6381018525bdade703abf60146d27e0
GemechisTaasisa <G.TAASISA-2021@hull.ac.uk>
2024-10-09 22:16:48 +0100

    CRUD DB

---
+++ StoreMVC/StoreMVC/Services/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreMVC.Models;
+
+namespace StoreMVC.Services
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options):base(options)
+        {
+             
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
